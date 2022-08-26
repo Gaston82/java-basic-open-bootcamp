8686443f1701f101ce5f723e879af9acbfd061ff
Gaston82 <cabezaartaza@hotmail.com>
2022-08-26 02:58:27 +0200

    first commit

---
+++ src/com/garya/Ejercicio3.java
@@ -0,0 +1,11 @@
+package com.garya;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+
+        String[] names = {"Ringo", "Paul", "George", "John"};
+        for (String name : names) {
+            System.out.print(name + " ");
+        }
+    }
+}
